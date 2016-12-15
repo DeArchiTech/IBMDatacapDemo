0203d8458852cb983acff472056e6ee9e78afe0a
noobiehacker <davidkwokhochan@gmail.com>
2016-12-15 12:10:32 -0800

    Set Up Unit Test

---
+++ IBMCaptureSDK-Sample/BoxService.swift
@@ -0,0 +1,9 @@
+//
+//  BoxService.swift
+//  IBMCaptureSDK-Sample
+//
+//  Created by davix on 12/15/16.
+//  Copyright © 2016 Future Workshops. All rights reserved.
+//
+
+import Foundation
---
+++ IBMCaptureSDK-SampleTests/IBMCaptureSDK_SampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  IBMCaptureSDK_SampleTests.swift
+//  IBMCaptureSDK-SampleTests
+//
+//  Created by davix on 12/15/16.
+//  Copyright © 2016 Future Workshops. All rights reserved.
+//
+
+import XCTest
+import IBMCaptureSDK_Sample
+
+class IBMCaptureSDK_SampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertTrue(false)
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+    }
+    
+}
