ff2d961f8d088d3fa9fa41f36f9ace7e88b92650
noobiehacker <davidkwokhochan@gmail.com>
2016-12-16 17:16:21 -0800

    Allowed Box SDK to be tested in Unit Test

--- IBMCaptureSDK-SampleTests/BoxServiceTest.swift
+++ IBMCaptureSDK-SampleTests/BoxServiceTest.swift
@@ -25,6 +25,7 @@ class BoxServiceTest: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         XCTAssertTrue(false)
+        let service = BoxService()
     }
     
     func testPerformanceExample() {
